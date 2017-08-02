a14cc7e87ece2417aef2666c27631bf9e7171ea4
Irina Zhelokhovtseva <zirina72@gmail.com>
2017-08-01 19:20:33 -0700

    Initial commit

---
+++ src/test/java/core/JUnitTest.java
@@ -0,0 +1,32 @@
+package core;
+
+import org.junit.*;
+
+public class JUnitTest {
+
+	@BeforeClass
+public static void beforeClass() {System.out.println("Before Class");}
+	
+	@AfterClass
+	public static void afterClass() {System.out.println("After Class");}
+
+	@Before
+	public void before()             {System.out.println("\tBefore Test");}
+
+	@After
+	public void after()              {System.out.println("\tAfter Test");}
+
+	@Test
+	public void test_01()            {System.out.println("\t\tTest # 01");}
+
+	@Test
+	public void test_02()            {System.out.println("\t\tTest # 02");}
+
+	@Test
+	public void test_03()            {System.out.println("\t\tTest # 03");}
+
+	@Test
+	public void test_04()            {System.out.println("\t\tTest # 04");}
+	
+	
+}
